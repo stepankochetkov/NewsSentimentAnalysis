93eb27412fba8d86d6ab524dc98aee5c405d193a
stepankochetkov@gmail.com <stepankochetkov@gmail.com>
2022-01-20 09:30:58 +0000

    Added Helper functions, Connection to the blob storage

--- Code/Databricks/silver_raw_to_silver_transformed/GoogleNews.py
+++ Code/Databricks/silver_raw_to_silver_transformed/GoogleNews.py
@@ -1,2 +1,45 @@
 # Databricks notebook source
-# comment here
+from pyspark.sql.types import *
+
+# COMMAND ----------
+
+storageAccountName = "storageskdev0001"
+storageAccountAccessKey = "."
+containerName = "devdata"
+
+# COMMAND ----------
+
+spark.conf.set(
+  f"fs.azure.account.key.{storageAccountName}.blob.core.windows.net",
+  f"{storageAccountAccessKey}")
+
+# COMMAND ----------
+
+dbutils.fs.ls(f"wasbs://{containerName}@{storageAccountName}.blob.core.windows.net/silver raw")
+
+# COMMAND ----------
+
+def get_dir_content(ls_path):
+  dir_paths = dbutils.fs.ls(ls_path)
+  subdir_paths = [get_dir_content(p.path) for p in dir_paths if p.isDir() and p.path != ls_path]
+  flat_subdir_paths = [p for subdir in subdir_paths for p in subdir]
+  return list(map(lambda p: p.path, dir_paths)) + flat_subdir_paths
+    
+
+paths = get_dir_content(f"wasbs://{containerName}@{storageAccountName}.blob.core.windows.net/silver raw")
+[print(p) for p in paths]
+
+# COMMAND ----------
+
+schemaGoogleNews = StructType([\
+                              StructField("status", StringType(), False),\
+                              StructField("totalResults", IntegerType(), False), \
+                              StructField("id")])
+
+# COMMAND ----------
+
+df = spark.read.parquet("wasbs://devdata@storageskdev0001.blob.core.windows.net/silver raw/Google News/Russia OR Russian/2022-01-17/Results_Russia OR Russian_2022-01-17.parquet")
+
+# COMMAND ----------
+
+display(df)
